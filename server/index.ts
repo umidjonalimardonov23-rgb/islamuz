041b07ecc18204eff81a467db0994af4e79fe4db
umidjonalimardonov23-rgb <umidjonalimardonov23@gmail.com>
2026-06-15 11:22:17 +0500

    fix: use setupBot(app) in server entry

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,7 @@
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
-import { startBot } from "./bot";
+import { setupBot } from "./bot";
 
 const app = express();
 app.use(express.json());
@@ -38,7 +38,7 @@ app.use((req, res, next) => {
 });
 
 (async () => {
-  startBot();
+  setupBot(app);
   const server = await registerRoutes(app);
 
   app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
